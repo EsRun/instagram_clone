3e64607dbad81cac9af0105bcee18b5025146a54
비엔나 <sure@paran.com>
2023-03-23 21:08:10 +0900

    add : router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./style/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <BrowserRouter>
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
