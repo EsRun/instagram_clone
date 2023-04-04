0c2ea720b40a9e9bb748b2889ba69dfae15c2101
비엔나 <sure@paran.com>
2023-04-04 15:28:52 +0900

    add : SideNav, Contents component

---
+++ src/components/SideNav.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const SideNav = () => {
+  return (
+    <>
+      <div>
+        <div>instagram</div>
+        <div>
+          <ul>
+            <li>홈</li>
+            <li>검색</li>
+            <li>탐색 탭</li>
+            <li>릴스</li>
+            <li>메시지</li>
+            <li>알림</li>
+            <li>만들기</li>
+            <li>프로필</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideNav;
+<ul>
+  <li>홈</li>
+</ul>;
