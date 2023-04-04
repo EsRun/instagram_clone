e7a234ca55e2053e2adc23c8a8a0bda53f739272
비엔나 <sure@paran.com>
2023-04-04 16:07:47 +0900

    add : Main layout in component

---
+++ src/layouts/Main.js
@@ -0,0 +1,12 @@
+import { SideNav, Contents } from "../components";
+
+const Main = () => {
+  return (
+    <>
+      <SideNav></SideNav>
+      <Contents></Contents>
+    </>
+  );
+};
+
+export default Main;
